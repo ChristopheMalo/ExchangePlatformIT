fe6e4812c9e6e825bec09e10b65134959374bf28
ChristopheMalo <christof.malo@gmail.com>
2016-03-16 21:32:02 +0100

    feat: code the authentification to the site

---
+++ src/OC/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OC\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+/**
+ * Class to display vie login form
+ *
+ * @author      Christophe Malo
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Alexandre Bacco
+ */
+
+class SecurityController extends Controller
+{
+
+    public function loginAction(Request $request)
+    {
+        // If the visitor is already logged in , he's redirected to the Home
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
+            return $this->redirectToRoute('oc_platform_accueil');
+        }
+
+        // The authentication_utils Service retrieves the user name
+        // and error if the form has been submitted but was invalid
+        // (wrong password for example)
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('OCUserBundle:Security:login.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+
+}
